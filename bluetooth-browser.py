20d7767bc365d32f5d0bc1358649e5a8a75b6b9d
Thorsten Wißmann <edu@thorsten-wissmann.de>
2019-01-28 18:30:56 +0100

    Show power and scan status

--- bluetooth-browser.py
+++ bluetooth-browser.py
@@ -6,6 +6,7 @@ from gi.repository import GObject
 from dbus.mainloop.glib import DBusGMainLoop
 from xml.etree import ElementTree
 
+
 def dbus_child_paths(obj):
     """return the names of all children of a given object_path"""
     iface = dbus.Interface(obj, 'org.freedesktop.DBus.Introspectable')
@@ -13,11 +14,6 @@ def dbus_child_paths(obj):
     for child in ElementTree.fromstring(xml_string):
         if child.tag == 'node':
             yield child.attrib['name']
-
-class BluetoothDevice:
-    """Wraper around a device that can be accessed via bluetooth"""
-    def __init__(self):
-        pass
 
 
 class DeviceWidget(urwid.Text):
@@ -33,8 +29,6 @@ class DeviceWidget(urwid.Text):
         return self.properties.GetAll('org.bluez.Device1')
 
     def getDisplayLabel(self):
-        def bool2str(b):
-            return 'x' if b else ' '
         p = self.getBluezProps()
         s = '{} ({})'.format(p['Name'], p['Address'])
         s += '\n   '
@@ -43,12 +37,11 @@ class DeviceWidget(urwid.Text):
             'connected' if p['Connected'] else 'disconnected')
         return s
 
-
 class BluetoothBrowser:
     """Bluetooth browser main widget"""
     palette = [
-        ('device normal','', '', 'standout'),
-        ('device select', 'black', 'dark green'),
+        ('item normal','', '', 'standout'),
+        ('item select', 'black', 'dark green'),
         ]
     def __init__(self, bus, service, hci_path):
         """
@@ -60,8 +53,6 @@ class BluetoothBrowser:
         self.hci_path = hci_path
         self.service = service
         self.hci_object = bus.get_object(service, self.hci_path)
-        self.device_widgets = [self.create_device_item(p)
-                               for p in self.bluetooth_device_paths()]
 
     def bluetooth_device_paths(self):
         for s in dbus_child_paths(self.hci_object):
@@ -69,13 +60,33 @@ class BluetoothBrowser:
 
     def setup_view(self):
         """return a widget as the root widget"""
-        self.devices_walker = urwid.SimpleFocusListWalker(self.device_widgets)
+        self.powered_checkbox = urwid.CheckBox("power")
+        self.powered_checkbox = urwid.AttrWrap(self.powered_checkbox, \
+                            'item normal', 'item select')
+        self.scanning_checkbox = urwid.CheckBox("scanning")
+        self.scanning_checkbox = urwid.AttrWrap(self.scanning_checkbox, \
+                            'item normal', 'item select')
+        self.list_widgets = \
+            [self.powered_checkbox,
+             self.scanning_checkbox] \
+            + [self.create_device_item(p)
+             for p in self.bluetooth_device_paths()]
+        self.devices_walker = urwid.SimpleFocusListWalker(self.list_widgets)
         self.devicesList = urwid.ListBox(self.devices_walker)
+        self.update_status()
         return self.devicesList
+
+    def update_status(self):
+        prop_iface = dbus.Interface(self.hci_object, 'org.freedesktop.DBus.Properties')
+        self.properties = prop_iface.GetAll('org.bluez.Adapter1')
+        self.powered_checkbox.set_state(self.properties['Powered'],
+            do_callback=False)
+        self.scanning_checkbox.set_state(self.properties['Discovering'],
+            do_callback=False)
 
     def create_device_item(self, object_path):
         w = DeviceWidget(self.bus.get_object(self.service, object_path))
-        w = urwid.AttrWrap(w, 'device normal', 'device select')
+        w = urwid.AttrWrap(w, 'item normal', 'item select')
         return w
 
     def unhandled_input(self, key):
